0fe6237f2ed774f89bc82060e9c2754c1098d680
Juliana Manzieri <julianaluckmann@hotmail.com>
2024-11-07 17:42:45 +0100

    first commit

---
+++ src/test/java/com/julianamanzieri/literalura/api/LiterAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianamanzieri.literalura.api.LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
